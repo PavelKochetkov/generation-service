4eb13462ad03dbd3d7b522e2f8d67c00acee3ca4
Pavel Kochetkov <kochetkov_p@mail.ru>
2023-07-26 16:19:26 +0300

    fix style

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -6,34 +6,34 @@ export default function HomePage (props) {
         <React.Fragment>
             <div className='home'>
                 <div className="logoPage">
-                    <img src={props.home.logo.img} alt={props.home.logo.alt}/>
+                  <img src={props.home.logo.img} alt={props.home.logo.alt}/>
                 </div>
                 <div className='info'>{props.home.info}</div>
                 <div className='down'>
-                    <img src={props.home.down.img} alt={props.home.down.alt}/>
+                  <img src={props.home.down.img} alt={props.home.down.alt}/>
                 </div>
             </div>
             <div className="description">
                 <div className="descrTitle">Для чего нужен сервис?</div>
                 <div className="genereateInfo">
-                    <div className="genPass">
+                    <div className="block">
                         <img 
-                            src={props.home.description.password.img} 
-                            alt={props.home.description.password.alt}
+                          src={props.home.description.password.img} 
+                          alt={props.home.description.password.alt}
                         />    
                         <div>{props.home.description.password.title}</div>
                     </div>
-                    <div className="genNum">
+                    <div className="block">
                         <img 
-                            src={props.home.description.numbers.img} 
-                            alt={props.home.description.numbers.alt}
+                          src={props.home.description.numbers.img} 
+                          alt={props.home.description.numbers.alt}
                         />    
                         <div>{props.home.description.numbers.title}</div>
                     </div>
-                    <div className="genOther">
+                    <div className="block">
                         <img 
-                            src={props.home.description.others.img} 
-                            alt={props.home.description.others.alt}
+                          src={props.home.description.others.img} 
+                          alt={props.home.description.others.alt}
                         />    
                         <div>{props.home.description.others.title}</div>
                     </div>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import HomePage from '../Components/HomePage'
-import { baseURL } from '../config'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import HomePage from "../Components/HomePage";
+import { baseURL } from "../config";
+import axios from "axios";
 
 export const Home = () => {
-    const [homes, setHome] = useState([])
-    useEffect(() => {
-        const fetchData = async () => {
-            const respHome = await axios.get(`${baseURL}homepage.json`)
-            setHome(respHome.data)
-        }
-        fetchData()
-    }, [])
-    return(
-        <React.Fragment>
-            {homes.map((home, index) => <div key={index}><HomePage home={home}/></div>)}
-        </React.Fragment>
-    )
-}
+  const [homes, setHome] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const respHome = await axios.get(`${baseURL}homepage.json`);
+      setHome(respHome.data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <React.Fragment>
+      {homes.map((home, index) => (
+        <HomePage home={home} key={index} />
+      ))}
+    </React.Fragment>
+  );
+};
