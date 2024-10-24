d04360ec9f087f1daa11c864403b33d237ebab48
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-10-24 20:59:33 +0300

    Navmenu component: implemented getting page path by key

--- src/Components/Navmenu.jsx
+++ src/Components/Navmenu.jsx
@@ -2,25 +2,26 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import '../css/navmenu.css';
 import { NavLink } from 'react-router-dom';
+import { getPageRoute } from '../utils/routes';
 
 const Navmenu = () => {
   const { t } = useTranslation();
 
   return (
     <nav className="menu">
-      <NavLink to="/" className="activ">
+      <NavLink to={getPageRoute('HOME_PAGE')} className="activ">
         <div>{t('navigation.main')}</div>
       </NavLink>
-      <NavLink to="/password" className="activ">
+      <NavLink to={getPageRoute('PASSWORD_PAGE')} className="activ">
         <div>{t('navigation.password')}</div>
       </NavLink>
-      <NavLink to="/numbers" className="activ">
+      <NavLink to={getPageRoute('NUMBER_PAGE')} className="activ">
         <div>{t('navigation.numbers')}</div>
       </NavLink>
-      <NavLink to="/qrservice" className="activ">
+      <NavLink to={getPageRoute('QR_PAGE')} className="activ">
         <div>{t('navigation.qr')}</div>
       </NavLink>
-      <NavLink to="/updates" className="activ">
+      <NavLink to={getPageRoute('UPDATE_PAGE')} className="activ">
         <div>{t('navigation.updates')}</div>
       </NavLink>
     </nav>
